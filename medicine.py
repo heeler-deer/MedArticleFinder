23630bb3dbc5d94ab441107d1c55c0632282b833
heeler-deer <heelerdeer@gmail.com>
2023-09-27 17:17:36 +0800

    9_27

--- medicine.py
+++ medicine.py
@@ -62,7 +62,7 @@ file_handler.setFormatter(file_formatter)
 logger.addHandler(console_handler)
 logger.addHandler(file_handler)
 
-article_info_list = []
+
 
 
 class ArticleInfo:
@@ -122,7 +122,7 @@ def get_IF_from_name(name):
         + "&research_area=&If_range_min=&If_range_max=&jcr_quartile=0&oa=2&Self_cites_ratio_min=&Self_cites_ratio_max=&mainclass=0&subclass=0&pub_country=&not_pub_country=&sci_type=2&pub_frequency=7&adv=1"
     )
     logger.info(address)
-    f = requests.get(address, headers=headers, timeout=(3, 7))
+    f = requests.get(address, headers=headers, timeout=(10, 50))
 
     parsed_html = html.fromstring(f.content)
 
@@ -140,7 +140,7 @@ def get_IF_from_name(name):
             row_data2 = str(row_data2[0])
         except Exception as e:
             return "N"
-
+        
         logger.info(row_data.strip().upper())
 
         logger.info(name.strip().upper())
@@ -148,7 +148,7 @@ def get_IF_from_name(name):
             row_data.strip().upper() == name.strip().upper()
             or row_data2.strip().upper() == name.strip().upper()
         ):
-            time.sleep(3)
+            time.sleep(0)
             logger.info(tr.xpath("td[7]/text()")[0])
 
             return tr.xpath("td[7]/text()")[0]
@@ -165,12 +165,12 @@ def get_article_by_keyword(key, key2):
     }
     logger.info("**********Getting pmid of article**********")
     for key3 in key_year:
-        address = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=2000&term=%5bjournal%5d+AND+"
+        address = "https://eutils.ncbi.nlm.nih.gov/entrez/eutils/esearch.fcgi?db=pubmed&retmax=3000&term=%5bjournal%5d+AND+"
         # address = address + key + "+" + key2 + "+AND+" + str(key3)
-        address = address + key + "+" + key2 + "+AND+" + \
-            "(%222012/01/01%22[Date%20-%20Publication]%20:%20%222023/12/31%22[Date%20-%20Publication])"
+        address = address + key  + "+AND+" + \
+            "(%221800/01/01%22[Date%20-%20Publication]%20:%20%222023/12/31%22[Date%20-%20Publication])"
         logger.info(address)
-        f = requests.get(address, headers=headers, timeout=(3, 7))
+        f = requests.get(address, headers=headers, timeout=(10, 50))
         soup = BeautifulSoup(f.text, features="xml")
 
         count = soup.find("Count").text
@@ -181,9 +181,9 @@ def get_article_by_keyword(key, key2):
             + "  |  "
             + str(key)
             + "  |  "
-            + str(key2)
+            + str("")
             + "  |  "
-            + str(key3)
+            + str("")
             + "  |"
         )
         if count == "0":
@@ -204,13 +204,13 @@ def get_article_by_keyword(key, key2):
             pmid_result_list.append(id_element.text)
         logger.info("%s", pmid_result_list)
 
-        time.sleep(3)
+        time.sleep(0)
 
     return pmid_result_list
 
 
-def get_details_by_pmid(pmid_list, key, key2):
-
+def get_details_by_pmid(pmid_list, matching_keywords, category):
+    article_info_list=[]
     headers = {
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/49.0.2623.22 Safari/537.36 SE 2.X MetaSr 1.0"
     }
@@ -225,7 +225,7 @@ def get_details_by_pmid(pmid_list, key, key2):
         + "&fl=pmid,year,title,authors,journal,relative_citation_ratio,nih_percentile,citation_count,field_citation_rate,doi"
     )
     logger.info(address)
-    f = requests.get(address, headers=headers, timeout=(3, 7))
+    f = requests.get(address, headers=headers, timeout=(10, 50))
     response = f.text
     soup = json.loads(response)
 
@@ -233,7 +233,9 @@ def get_details_by_pmid(pmid_list, key, key2):
         logger.info("PMID:" + str(item["pmid"]))
 
         logger.info("Year:" + str(item["year"]))
-        logger.info("Title:" + item["title"])
+        title= re.sub(r"<.*?>", "", item["title"])
+        title=re.sub(r"[.*?]", "", title)
+        logger.info("Title:" + title)
         logger.info("Authors:" + item["authors"])
         logger.info("Journal:" + item["journal"])
         logger.info("Relative Citation Ratio:" +
@@ -252,31 +254,31 @@ def get_details_by_pmid(pmid_list, key, key2):
             str(item["pmid"]) + "/"
 
         abstract, keywords = get_abstract_keywords_by_pmid(item["pmid"])
-
-        logger.info(key)
-        category = ""
-        if (
-            key == "dandruff"
-            or key == "sebum"
-            or key == "Seborrheic Dermatitis"
-            or key == "inflammation"
-            or key == "fungi"
-            or key == "Malassezia"
-            or key == "microbes"
-        ):
-            category = "reason"
-        elif key == "greasy" or key == "greasiness":
-            category = "reason,symptom"
-        elif key == "flaking" or key == "itch" or key == "dry" or key == "oily":
-            category = "symptom"
-        elif (
-            key == "zinc"
-            or key == "ZPT"
-            or key == "octopirox"
-            or key == "climbazole"
-            or key == "Ketoconazole"
-        ):
-            category = "solution"
+        #TODO  key is list , but now it is a str
+        logger.info(matching_keywords)
+        category = category
+        # if (
+        #     key == "dandruff"
+        #     or key == "sebum"
+        #     or key == "Seborrheic Dermatitis"
+        #     or key == "inflammation"
+        #     or key == "fungi"
+        #     or key == "Malassezia"
+        #     or key == "microbes"
+        # ):
+        #     category = "reason"
+        # elif key == "greasy" or key == "greasiness":
+        #     category = "reason,symptom"
+        # elif key == "flaking" or key == "itch" or key == "dry" or key == "oily":
+        #     category = "symptom"
+        # elif (
+        #     key == "zinc"
+        #     or key == "ZPT"
+        #     or key == "octopirox"
+        #     or key == "climbazole"
+        #     or key == "Ketoconazole"
+        # ):
+        #     category = "solution"
 
         logger.info(category)
 
@@ -293,7 +295,7 @@ def get_details_by_pmid(pmid_list, key, key2):
         article_info = ArticleInfo(
             item["pmid"],
             item["year"],
-            item["title"],
+            title,
             item["authors"],
             item["journal"],
             item["relative_citation_ratio"],
@@ -304,13 +306,13 @@ def get_details_by_pmid(pmid_list, key, key2):
             abstract,
             keywords,
             source_url,
-            matching_keywords=key,
+            matching_keywords=matching_keywords,
             category=category,
             hindex=hindex,
             doi=item['doi'],
             document_url=document_url
         )
-
+        
         article_info_list.append(article_info)
 
     data = []
@@ -364,9 +366,10 @@ def get_details_by_pmid(pmid_list, key, key2):
         ],
     )
 
-    csv_filename = "article_info.csv"
-    #df.to_csv(csv_filename, mode='a', header=False, index=False)
-    df.to_csv(csv_filename, mode='w', header=True, index=False)
+    csv_filename = "dandruff_info.csv"
+    file_exists = pd.io.common.file_exists(csv_filename)
+    df.to_csv(csv_filename, mode='a', header=not file_exists, index=False)
+    #df.to_csv(csv_filename, mode='w', header=True, index=False)
     logger.info("***************************")
     logger.info("**********Success**********")
     logger.info("***************************")
@@ -389,7 +392,11 @@ def get_abstract_keywords_by_pmid(pmid):
         + "&retmode=XML"
     )
     logger.info(address)
-    f = requests.get(address, headers=headers, timeout=(3, 7))
+    try:
+        f = requests.get(address, headers=headers, timeout=(15, 50))
+    except Exception as e:
+        return "",""
+        
     soup = BeautifulSoup(f.text, features="xml")
     raw_abstract = soup.find("AbstractText")
     if raw_abstract == None:
@@ -419,7 +426,6 @@ def get_hindex_by_author(name):
     logger.info("**********Getting hindex by author**********")
     try:
         search_query = scholarly.search_author(name)
-
         author = next(search_query)
 
         scholarly.pprint(scholarly.fill(author, sections=["indices"]))
@@ -437,26 +443,90 @@ def get_hindex_by_author(name):
         return hindex
 
 
+def map_keys_to_categories(input_str):
+    key_to_category = {
+        "ZPT": "solution",
+        "zinc": "solution",
+        "octopirox": "solution",
+        "climbazole": "solution",
+        "Ketoconazole": "solution",
+        "greasy": "reason,symptom",
+        "greasiness": "reason,symptom",
+        "flaking": "symptom",
+        "itch": "symptom",
+        "dry": "symptom",
+        "oily": "symptom",
+        "dandruff": "reason",
+        "sebum": "reason",
+        "Seborrheic Dermatitis": "reason",
+        "inflammation": "reason",
+        "fungi": "reason",
+        "Malassezia": "reason",
+        "microbes": "reason",
+        "piroctone olamine": 'solution',
+    }
+
+    keys = input_str.split(',')
+    categories = [key_to_category.get(key, "") for key in keys]
+
+    return ','.join(categories)
+
 def main():
-    key_list = ["dandruff", "sebum", "Seborrheic Dermatitis", "inflammation",
-                "fungi", "Malassezia", "microbes", "greasy", "greasiness",
-                "flaking", "itch", "dry", "oily", "zinc", "ZPT",
-                "octopirox", "climbazole", "Ketoconazole"]
-    key_second_list = ["shampoo","hair conditioner","scalp cleanser"]
+    # key_list = ["dandruff", "sebum", "Seborrheic Dermatitis", "inflammation",
+    #             "fungi", "Malassezia", "microbes", "greasy", "greasiness",
+    #             "flaking", "itch", "dry", "oily", "zinc", "ZPT",
+    #             "octopirox", "climbazole", "Ketoconazole","shampoo","hair conditioner","scalp cleanser"]
+    key_list = ["dandruff"
+                
+                ]
+    key_second_list = [""]
+    pmid_list_map={}
+    
     for key in key_list:
         key_pmid_list=["placeholder"]
         for key2 in key_second_list:
             
             pmid_list = get_article_by_keyword(key, key2)
-            if pmid_list[0] == "error" or key_pmid_list is None or pmid_list is  None:
-                logger.error("error")
-                logger.error(key_pmid_list)
-                logger.error(pmid_list)
-                continue
-            if key_pmid_list is not None and pmid_list is not None:
-                pmid_list=[x for x in pmid_list if x not in key_pmid_list]
-                key_pmid_list=key_pmid_list.extend(pmid_list)
-                get_details_by_pmid(pmid_list=pmid_list, key=key, key2=key2)
+            
+            for pmid in pmid_list:
+                if pmid in pmid_list_map:
+                    pmid_list_map[pmid].append(key)
+                else:
+                    pmid_list_map[pmid] = list([key])
+    file_name = "pmid_list_map_dandruff.txt"
+
+    with open(file_name, "w") as file:
+        for key, value in pmid_list_map.items():
+            file.write(f"{key}: {value}\n")
+    cnt=0
+    
+    cur_index=100
+    
+    
+    
+    for pmid in pmid_list_map.keys():
+        
+        
+        matching_keywords=pmid_list_map[pmid]
+        logger.info(pmid)
+        pmid_list=[]
+        pmid_list.append(pmid)
+        logger.info(pmid_list)
+        #TODO now str_matching_keywords only has one matching_keywords
+        str_matching_keywords=", ".join(matching_keywords)
+        category=map_keys_to_categories(str_matching_keywords)
+        logger.info(str_matching_keywords)
+        logger.info(category)
+        
+        
+        
+        cnt+=1
+        get_details_by_pmid(pmid_list=pmid_list, matching_keywords=str_matching_keywords, category=category)
+
+    logger.info(cnt)
+
+    
+
 
 
 if __name__ == "__main__":
